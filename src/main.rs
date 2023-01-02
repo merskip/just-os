8a92f2f3522f5fc1e864ab9911d3575a93c6068b
Piotr Merski <merskip@gmail.com>
2023-01-02 15:10:43 +0100

    Added marcors for print/println

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,16 @@
 use core::panic::PanicInfo;
 
 mod vga_buffer;
-use vga_buffer::{Writer, ColorCode, Color, Buffer};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    println!("Hello World{}", "!");
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("[PANIC!]");
+    println!("{}", info);
     loop {}
 }
