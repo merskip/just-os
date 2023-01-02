b2da33a38c1b808e2e2d7e7358dba452ca8dcddc
Piotr Merski <merskip@gmail.com>
2023-01-02 12:38:34 +0100

    Addef vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,15 @@
 
 use core::panic::PanicInfo;
 
+use vga_buffer::print_something;
+
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Hello world!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    print_something();
     loop {}
 }
 
@@ -22,3 +19,4 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
