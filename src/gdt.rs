1d255d7cdab91001445b890409f6d30c90ee6172
Piotr Merski <merskip@gmail.com>
2023-01-04 20:30:12 +0100

    Some refactor

--- src/gdt.rs
+++ src/gdt.rs
@@ -4,8 +4,8 @@ use x86_64::instructions::segmentation::{CS, Segment};
 use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor, SegmentSelector};
 
 struct Selectors {
-    code_selector: SegmentSelector,
-    tss_selector: SegmentSelector,
+    code: SegmentSelector,
+    tss: SegmentSelector,
 }
 
 lazy_static! {
@@ -13,16 +13,17 @@ lazy_static! {
         let mut gdt = GlobalDescriptorTable::new();
         let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
         let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
-        (gdt, Selectors { code_selector, tss_selector })
+        (gdt, Selectors { code: code_selector, tss: tss_selector })
     };
 }
 
 pub fn init() {
-    GDT.0.load();
+    let (ref gdt, ref selectors) = *GDT;
     
+    gdt.load();
     unsafe {
-        CS::set_reg(GDT.1.code_selector);
-        load_tss(GDT.1.tss_selector);
+        CS::set_reg(selectors.code);
+        load_tss(selectors.tss);
     }
 }
 
