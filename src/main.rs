5b93f558d81b0093bb15afa781e73e1d60a98e84
Piotr Merski <merskip@gmail.com>
2023-01-06 14:53:41 +0100

    Added LinkedListAllocator

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,26 @@
 
 extern crate alloc;
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use alloc::boxed::Box;
-use bootloader::{BootInfo, entry_point};
+use x86_64::VirtAddr;
 
-mod memory;
 mod allocator;
-mod video;
-mod interrupts;
 mod gdt;
+mod interrupts;
+mod memory;
+mod video;
 
 entry_point!(kernel_main);
 
 #[no_mangle]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello {}!", "world");
-    
-    let mut frame_allocator = unsafe {
-        memory::BootInfoFrameAllocator::new(&boot_info.memory_map)
-    };
 
-
-    let x = Box::new(41);
+    let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(physical_memory_offset) };
+    let mut frame_allocator = unsafe { memory::BootInfoFrameAllocator::new(&boot_info.memory_map) };
+    allocator::init(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
     interrupts::init();
     gdt::init();
@@ -44,7 +42,7 @@ fn alloc_error_handler(layout: alloc::alloc::Layout) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("[PANIC!]");
     println!("{}", info);
-    
+
     interrupts::disable();
     interrupts::halt_loop();
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,12 +1,27 @@
 use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
-use x86_64::{structures::paging::{PhysFrame, FrameAllocator, Size4KiB}, PhysAddr};
+use x86_64::{structures::paging::{PhysFrame, FrameAllocator, Size4KiB, OffsetPageTable, PageTable}, PhysAddr, VirtAddr};
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let phys = level_4_table_frame.start_address();
+    let virt = physical_memory_offset + phys.as_u64();
+    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+    &mut *page_table_ptr // unsafe
+}
 
 pub struct BootInfoFrameAllocator {
     memory_map: &'static MemoryMap,
     next: usize,
 }
-
-
 
 impl BootInfoFrameAllocator {
     pub unsafe fn new(memory_map: &'static MemoryMap) -> Self {
