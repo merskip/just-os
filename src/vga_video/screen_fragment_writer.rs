481d87e4cc63e77584f8edef5d865c6432eb85d5
Piotr Merski <merskip@gmail.com>
2023-02-03 12:05:45 +0100

    new_screen_fragment_writer

--- src/vga_video/screen_fragment_writer.rs
+++ src/vga_video/screen_fragment_writer.rs
@@ -43,19 +43,24 @@ impl Write for ScreenFragmentWriter<'_> {
 #[test_case]
 fn test_write_short_text() {
     use crate::vga_video::mock_frame_buffer::MockFrameBuffer;
-    use crate::geometry::size::Size;
-    use crate::geometry::position::Point;
 
     let mut frame_buffer= MockFrameBuffer::new(80, 25);
-    let mut writer = ScreenFragmentWriter::new(
-        Rect::new(Point::new(1, 1), Size::new(10, 10)),
-        CharacterColor::default(),
-        &mut frame_buffer,
-    );
+    let mut writer = new_screen_fragment_writer(&mut frame_buffer);
 
     writer.write_str("Abc").unwrap();
 
     assert_eq!(frame_buffer.get_character(1, 1), 'A');
     assert_eq!(frame_buffer.get_character(2, 1), 'b');
     assert_eq!(frame_buffer.get_character(3, 1), 'c');
+}
+
+#[cfg(test)]
+fn new_screen_fragment_writer(frame_buffer: &mut dyn FrameBuffer) -> ScreenFragmentWriter {
+    use crate::geometry::size::Size;
+
+    ScreenFragmentWriter::new(
+        Rect::new(Point::new(1, 1), Size::new(10, 10)),
+        CharacterColor::default(),
+        frame_buffer,
+    )
 }
