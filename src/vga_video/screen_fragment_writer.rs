a4d25ee97e1e4d7e3102616490a1f008dcfe2f55
Piotr Merski <merskip@gmail.com>
2023-02-04 10:46:33 +0100

    Added test_write_two_fragments

--- src/vga_video/screen_fragment_writer.rs
+++ src/vga_video/screen_fragment_writer.rs
@@ -1,4 +1,5 @@
 use alloc::boxed::Box;
+use core::cell::RefCell;
 use core::fmt::Write;
 use crate::error::Error;
 use crate::geometry::position::Point;
@@ -9,12 +10,12 @@ use crate::vga_video::frame_buffer::FrameBuffer;
 struct ScreenFragmentWriter<'a> {
     rect: Rect,
     default_color: CharacterColor,
-    frame_buffer: &'a mut dyn FrameBuffer,
+    frame_buffer: &'a RefCell<dyn FrameBuffer>,
     next_position: Point,
 }
 
 impl<'a> ScreenFragmentWriter<'a> {
-    pub fn new(rect: Rect, default_color: CharacterColor, frame_buffer: &'a mut dyn FrameBuffer) -> Self {
+    pub fn new(rect: Rect, default_color: CharacterColor, frame_buffer: &'a RefCell<dyn FrameBuffer>) -> Self {
         Self { rect, default_color, frame_buffer, next_position: rect.corner_upper_left() }
     }
 }
@@ -31,7 +32,8 @@ impl Write for ScreenFragmentWriter<'_> {
                     self.move_to_next_line();
                 }
                 _ => {
-                    self.frame_buffer.set_char(self.next_position, char, self.default_color)
+                    self.frame_buffer.borrow_mut()
+                        .set_char(self.next_position, char, self.default_color)
                         .unwrap();
                     self.move_to_next_position();
                 }
@@ -63,12 +65,13 @@ impl<'a> ScreenFragmentWriter<'a> {
     fn scroll_up(&mut self) -> Result<(), Box<dyn Error>> {
         let rows = self.rect.min_y()..=self.rect.max_y();
         let columns = self.rect.min_x()..=self.rect.max_x();
+        let mut frame_buffer = self.frame_buffer.borrow_mut();
 
         for row in rows.skip(1) {
             let source_row = row;
             let destination_row = row - 1;
             for column in columns.clone() {
-                self.frame_buffer.copy_char(
+                frame_buffer.copy_char(
                     Point::new(column, source_row),
                     Point::new(column, destination_row),
                 )?;
@@ -77,7 +80,7 @@ impl<'a> ScreenFragmentWriter<'a> {
 
         let last_row = self.rect.max_y();
         for column in columns {
-            self.frame_buffer.set_char(
+            frame_buffer.set_char(
                 Point::new(column, last_row),
                 char::default(),
                 CharacterColor::default(),
@@ -93,15 +96,16 @@ fn test_write_short_text() {
     use crate::vga_video::mock_frame_buffer::MockFrameBuffer;
     use crate::geometry::size::Size;
 
-    let mut frame_buffer = MockFrameBuffer::new(80, 25);
+    let frame_buffer_cell = RefCell::new(MockFrameBuffer::new(80, 25));
     let mut writer = ScreenFragmentWriter::new(
         Rect::new(Point::new(1, 1), Size::new(10, 3)),
         CharacterColor::default(),
-        &mut frame_buffer,
+        &frame_buffer_cell,
     );
 
     writer.write_str("Abc").unwrap();
 
+    let frame_buffer = frame_buffer_cell.borrow();
     assert_eq!(frame_buffer.get_chars(0, 0, 5), ['\0'; 5]);
     assert_eq!(frame_buffer.get_chars(0, 1, 5), ['\0', 'A', 'b', 'c', '\0']);
     assert_eq!(frame_buffer.get_chars(0, 2, 5), ['\0'; 5]);
@@ -112,17 +116,18 @@ fn test_write_multiline_text() {
     use crate::vga_video::mock_frame_buffer::MockFrameBuffer;
     use crate::geometry::size::Size;
 
-    let mut frame_buffer = MockFrameBuffer::new(80, 25);
+    let frame_buffer = RefCell::new(MockFrameBuffer::new(80, 25));
     let mut writer = ScreenFragmentWriter::new(
         Rect::new(Point::new(1, 1), Size::new(10, 3)),
         CharacterColor::default(),
-        &mut frame_buffer,
+        &frame_buffer,
     );
 
     writer.write_str("Lorem ipsu\
                              m dolor si\
                              t amet").unwrap();
 
+    let frame_buffer = frame_buffer.borrow();
     assert_eq!(frame_buffer.get_chars(0, 1, 12),
                ['\0', 'L', 'o', 'r', 'e', 'm', ' ', 'i', 'p', 's', 'u', '\0']);
     assert_eq!(frame_buffer.get_chars(0, 2, 12),
@@ -136,15 +141,16 @@ fn test_write_text_with_new_line() {
     use crate::vga_video::mock_frame_buffer::MockFrameBuffer;
     use crate::geometry::size::Size;
 
-    let mut frame_buffer = MockFrameBuffer::new(80, 25);
+    let frame_buffer = RefCell::new(MockFrameBuffer::new(80, 25));
     let mut writer = ScreenFragmentWriter::new(
         Rect::new(Point::new(1, 1), Size::new(10, 3)),
         CharacterColor::default(),
-        &mut frame_buffer,
+        &frame_buffer,
     );
 
     writer.write_str("Lorem\nipsum").unwrap();
 
+    let frame_buffer = frame_buffer.borrow();
     assert_eq!(frame_buffer.get_chars(0, 1, 12),
                ['\0', 'L', 'o', 'r', 'e', 'm', '\0', '\0', '\0', '\0', '\0', '\0']);
     assert_eq!(frame_buffer.get_chars(0, 2, 12),
@@ -156,15 +162,16 @@ fn test_write_text_with_scroll() {
     use crate::vga_video::mock_frame_buffer::MockFrameBuffer;
     use crate::geometry::size::Size;
 
-    let mut frame_buffer = MockFrameBuffer::new(80, 25);
+    let frame_buffer = RefCell::new(MockFrameBuffer::new(80, 25));
     let mut writer = ScreenFragmentWriter::new(
         Rect::new(Point::new(1, 1), Size::new(10, 3)),
         CharacterColor::default(),
-        &mut frame_buffer,
+        &frame_buffer,
     );
 
     writer.write_str("Lorem\nipsum\ndolor\nsit").unwrap();
 
+    let frame_buffer = frame_buffer.borrow();
     assert_eq!(frame_buffer.get_chars(0, 1, 12),
                ['\0', 'i', 'p', 's', 'u', 'm', '\0', '\0', '\0', '\0', '\0', '\0']);
     assert_eq!(frame_buffer.get_chars(0, 2, 12),
@@ -174,27 +181,31 @@ fn test_write_text_with_scroll() {
 }
 
 #[test_case]
-fn test_two_fragments() {
+fn test_write_two_fragments() {
     use crate::vga_video::mock_frame_buffer::MockFrameBuffer;
     use crate::geometry::size::Size;
 
-    let mut frame_buffer = MockFrameBuffer::new(80, 25);
+    let frame_buffer = RefCell::new(MockFrameBuffer::new(80, 25));
 
     let mut writer_1 = ScreenFragmentWriter::new(
         Rect::new(Point::new(1, 1), Size::new(10, 1)),
         CharacterColor::default(),
-        &mut frame_buffer,
+        &frame_buffer,
     );
 
-    // let mut writer_2 = ScreenFragmentWriter::new(
-    //     Rect::new(Point::new(1, 4), Size::new(10, 1)),
-    //     CharacterColor::default(),
-    //     &mut frame_buffer,
-    // );
+    let mut writer_2 = ScreenFragmentWriter::new(
+        Rect::new(Point::new(1, 3), Size::new(10, 1)),
+        CharacterColor::default(),
+        &frame_buffer,
+    );
 
     writer_1.write_str("Lorem").unwrap();
+    writer_2.write_str("ipsum").unwrap();
 
-    // writer_2.write_str("Ipusm").unwrap();
-
-    // TODO: asserts
+    let frame_buffer = frame_buffer.borrow();
+    assert_eq!(frame_buffer.get_chars(0, 0, 7), ['\0'; 7]);
+    assert_eq!(frame_buffer.get_chars(0, 1, 7), ['\0', 'L', 'o', 'r', 'e', 'm', '\0']);
+    assert_eq!(frame_buffer.get_chars(0, 2, 7), ['\0'; 7]);
+    assert_eq!(frame_buffer.get_chars(0, 3, 7), ['\0', 'i', 'p', 's', 'u', 'm', '\0']);
+    assert_eq!(frame_buffer.get_chars(0, 4, 7), ['\0'; 7]);
 }
